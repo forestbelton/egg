9c923832ea874259e41c54d23771c941dfda5860
Forest Belton <forest@homolo.gy>
2017-09-07 23:10:40 -0700

    Implement page router

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ var codeField = document.getElementById('code')
 var inputField = document.getElementById('input')
 var outputField = document.getElementById('output')
 
+function changePage(ev) {
+    const page = (ev && ev.target.href.replace(/.*#/, ''))
+        || (window.location.hash && window.location.hash.replace(/.*#/, ''))
+        || 'interpreter'
+
+    const activeLink = document.querySelector('.nav .active')
+    if (activeLink !== null) {
+        activeLink.className = ''
+    }
+    document.querySelector(`a[href="#${page}"]`).className = 'active'
+
+    const activePage = document.querySelector('.page.visible')
+    if (activePage !== null) {
+        activePage.className = 'page'
+    }
+
+    document.getElementById(page).className = 'page visible'
+}
+
+document.querySelectorAll('.nav a').forEach(navLink =>
+    navLink.addEventListener('click', changePage)
+)
+
+changePage()
+
 inputForm.addEventListener('submit', ev => {
     ev.preventDefault()
 
