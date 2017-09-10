b64563f6bbb0ab3ed7b427b5abc239ef2abf29ad
Forest Belton <forest@homolo.gy>
2017-09-10 01:24:44 -0700

    Add more assertions to addition test

--- test/operator/plus_test.js
+++ test/operator/plus_test.js
@@ -1,11 +1,13 @@
 import { expect } from 'chai'
 import Context from '../../lib/Context'
 
-describe('+', function() {
+describe('+ operator', function() {
     it('should add two bigints', function() {
         const context = new Context('1 2 +', '')
         context.execute()
 
         expect(context.stack).to.have.length(1)
+        expect(context.stack[0].type).to.equal('bigint')
+        expect(context.stack[0].value.toJSNumber()).to.equal(3)
     })
 })
