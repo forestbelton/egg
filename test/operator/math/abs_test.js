437ef0c4f5e618eabff390bf0451aa9833c79c1b
Forest Belton <forest@homolo.gy>
2017-09-10 10:33:11 -0700

    Fix clause precedence for absolute value

---
+++ test/operator/math/abs_test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import { term } from '../../helper'
+import Context from '../../../lib/Context'
+import abs from '../../../lib/operator/math/abs'
+
+describe('ma operator', function() {
+    it('should compute absolute value for bigints', function() {
+        const context = new Context()
+        context.stack = [term('bigint', -1)]
+
+        abs.execute(context)
+        expect(context.stack).to.have.length(1)
+        expect(context.stack[0].type).to.equal('bigint')
+        expect(context.stack[0].value.toJSNumber()).to.equal(1)
+    })
+
+    it('should compute absolute value for floats', function() {
+        const context = new Context()
+        context.stack = [term('float', -1)]
+
+        abs.execute(context)
+        expect(context.stack).to.have.length(1)
+        expect(context.stack[0].type).to.equal('float')
+        expect(context.stack[0].value).to.equal(1)
+    })
+})
