e825b6058eeb2ee7e6a3cfb6a31eee2ec1272feb
Forest Belton <forest@homolo.gy>
2017-09-10 10:44:01 -0700

    Add tests for remaining plus operations

--- test/operator/plus_test.js
+++ test/operator/plus_test.js
@@ -13,4 +13,31 @@ describe('+ operator', function() {
         expect(context.stack[0].type).to.equal('bigint')
         expect(context.stack[0].value.toJSNumber()).to.equal(3)
     })
+
+    it('should add two bigints', function() {
+        const context = new Context()
+        context.stack = [term('float', 1.5), term('float', 2)]
+
+        plus.execute(context)
+        expect(context.stack).to.deep.equal([{ type: 'float', value: 3.5 }])
+    })
+
+    it('should concatenate two arrays', function() {
+        const context = new Context()
+        const xs = [term('string', 'a')]
+        const ys = [term('string', 'b')]
+
+        context.stack = [term('array', xs), term('array', ys)]
+        plus.execute(context)
+
+        expect(context.stack).to.deep.equal([{ type: 'array', value: xs.concat(ys) }])
+    })
+
+    it('should concatenate two strings', function() {
+        const context = new Context()
+        context.stack = [term('string', 'abc'), term('string', 'def')]
+
+        plus.execute(context)
+        expect(context.stack).to.deep.equal([{ type: 'string', value: 'abcdef' }])
+    })
 })
