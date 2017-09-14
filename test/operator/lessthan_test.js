a707191b61a94436a417bddcaad2c8c01565307c
LiquidLemon <theprosail@gmail.com>
2017-09-14 12:00:19 +0200

    Add less than comparison tests

--- test/operator/lessthan_test.js
+++ test/operator/lessthan_test.js
@@ -5,6 +5,70 @@ import Context from '../../lib/runtime/Context'
 import lessthan from '../../lib/runtime/operator/lessthan'
 
 describe('< operator', function() {
+    it('should drop n first arguments from an array', function () {
+        const context = new Context()
+        context.stack = [
+            term('array', [
+                term('string', 'a'),
+                term('string', 'b'),
+                term('string', 'c')
+            ]),
+            term('float', 2)
+        ]
+
+        lessthan.execute(context)
+        expect(context.stack).to.deep.equal([{
+            type: 'array',
+            value: [
+                { type: 'string', value: 'c' }
+            ]
+        }])
+    })
+
+    it('should evaluate to 1 when float is less', function() {
+        const context = new Context()
+        context.stack = [term('float', 1), term('float', 2)]
+
+        lessthan.execute(context)
+        expect(context.stack).to.deep.equal([{
+            type: 'bigint',
+            value: bigInt[1]
+        }])
+    })
+
+    it('should evaluate to 0 when float is not less', function() {
+        const context = new Context()
+        context.stack = [term('float', 1), term('float', 0)]
+
+        lessthan.execute(context)
+        expect(context.stack).to.deep.equal([{
+            type: 'bigint',
+            value: bigInt[0]
+        }])
+    })
+
+    it('should evaluate to 1 when bigint is less', function() {
+        const context = new Context()
+        context.stack = [term('bigint', bigInt[1]), term('float', bigInt[2])]
+
+        lessthan.execute(context)
+        expect(context.stack).to.deep.equal([{
+            type: 'bigint',
+            value: bigInt[1]
+        }])
+    })
+
+    it('should evaluate to 0 when bigint is not less', function() {
+        const context = new Context()
+        context.stack = [term('bigint', bigInt[1]), term('float', bigInt[0])]
+
+        lessthan.execute(context)
+        expect(context.stack).to.deep.equal([{
+            type: 'bigint',
+            value: bigInt[0]
+        }])
+    })
+
     it('should evaluate to 1 when string less than', function() {
         const context = new Context()
         context.stack = [term('string', 'a'), term('string', 'b')]
