2bdbc2ed93019d4bdc463a15faba13e24da520a6
Forest Belton <forest@homolo.gy>
2017-09-18 12:50:40 -0700

    Finish implementing operator execution

--- lib/runtime/Runtime.purs
+++ lib/runtime/Runtime.purs
@@ -1,16 +1,18 @@
 module Egg.Runtime.Runtime where
 
-import Data.Array (head, filter)
+import Data.Array (head, filter, length, drop, all, zip)
 import Data.Foldable (foldr, foldMap)
 import Data.Map (lookup)
 import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
 import Partial.Unsafe (unsafeCrashWith)
-import Prelude ((<>), ($))
+import Prelude ((<>), ($), (>=), (&&), (-), (==))
 
 import Egg.Runtime.Operator.Operator (Clause)
 import Egg.Runtime.Operator.Table (operatorTable)
 import Egg.Runtime.Context (Context, newContext, push, getOutput, getStack)
 import Egg.Runtime.Token (Token(..), displayToken)
+import Egg.Runtime.Type (Ty, typeOf)
 
 evaluate :: String -> String -> String
 evaluate code input = output <> stack
@@ -25,8 +27,14 @@ evaluateToken (Op name) ctx = case lookup name operatorTable of
     Nothing -> unsafeCrashWith $ "unknown operator: " <> name
     Just op -> case head $ filter (matchingClause (getStack ctx)) op.clauses of
         Nothing -> unsafeCrashWith $ "no matching clause for: " <> name
-        Just clause -> unsafeCrashWith "poopy"
+        Just clause -> clause.body ctx
 evaluateToken value ctx = push ctx value
 
 matchingClause :: Array Token -> Clause -> Boolean
-matchingClause stack clause = false
+matchingClause stack clause = length stack >= length clause.sig
+    && sigMatches stack clause.sig
+
+sigMatches :: Array Token -> Array Ty -> Boolean
+sigMatches stack tys = all check $ zip top tys
+    where top = drop (length stack - length tys) stack
+          check (Tuple token ty) = typeOf token == ty
