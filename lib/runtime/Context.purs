006fe9034fa6fa0c195f53639feca50967a23faa
Forest Belton <forest@homolo.gy>
2017-09-17 21:17:40 -0700

    Add environment to runtime context

--- lib/runtime/Context.purs
+++ lib/runtime/Context.purs
@@ -2,23 +2,25 @@ module Egg.Runtime.Context where
 
 import Prelude (($))
 import Data.Array ((:), take, drop)
+import Data.Map (Map, empty)
 import Data.Tuple (Tuple(..))
 
 import Egg.Runtime.Stack (Stack(..))
 import Egg.Runtime.Token (Token)
 
-data Context = Context Stack String String
+type Env = Map String Token
+data Context = Context Env Stack String String
 
 newContext :: Stack -> String -> Context
-newContext stack input = Context stack input ""
+newContext stack input = Context empty stack input ""
 
 overStack :: forall a. (Array Token -> Tuple a (Array Token)) -> Context -> Tuple a Context
-overStack f (Context (Stack stack) input output) = Tuple x ctx
+overStack f (Context env (Stack stack) input output) = Tuple x ctx
     where (Tuple x stack') = f stack
-          ctx = Context (Stack stack') input output
+          ctx = Context env (Stack stack') input output
 
 push :: Context -> Token -> Context
-push (Context (Stack tokens) input output) token = Context stack' input output
+push (Context env (Stack tokens) input output) token = Context env stack' input output
     where stack' = Stack $ token : tokens
 
 pop :: Context -> Int -> Tuple (Array Token) Context
