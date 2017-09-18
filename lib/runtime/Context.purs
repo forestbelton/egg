3134d889b39ca70c025020bef0e8ab0f6785a097
Forest Belton <forest@homolo.gy>
2017-09-17 21:41:46 -0700

    Initialize default environment

--- lib/runtime/Context.purs
+++ lib/runtime/Context.purs
@@ -2,17 +2,16 @@ module Egg.Runtime.Context where
 
 import Prelude (($))
 import Data.Array ((:), take, drop)
-import Data.Map (Map, empty)
 import Data.Tuple (Tuple(..))
 
+import Egg.Runtime.Env (Env, defaultEnv)
 import Egg.Runtime.Stack (Stack(..))
-import Egg.Runtime.Token (Token)
+import Egg.Runtime.Token (Token(..))
 
-type Env = Map String Token
 data Context = Context Env Stack String String
 
 newContext :: Stack -> String -> Context
-newContext stack input = Context empty stack input ""
+newContext stack input = Context defaultEnv stack input ""
 
 overStack :: forall a. (Array Token -> Tuple a (Array Token)) -> Context -> Tuple a Context
 overStack f (Context env (Stack stack) input output) = Tuple x ctx
@@ -26,3 +25,7 @@ push (Context env (Stack tokens) input output) token = Context env stack' input 
 pop :: Context -> Int -> Tuple (Array Token) Context
 pop ctx n = overStack go ctx
     where go stack = Tuple (take n stack) (drop n stack)
+
+execute :: Context -> Token -> Context
+execute context (Op op) = context
+execute context value   = push context value
