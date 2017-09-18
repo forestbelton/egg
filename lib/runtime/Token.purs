7fda1dafe1fa26fe5789d5c91dd5206cea48f7a7
Forest Belton <forest@homolo.gy>
2017-09-17 19:27:37 -0700

    Add dependencies and stack type

--- lib/runtime/Token.purs
+++ lib/runtime/Token.purs
@@ -1,7 +1,9 @@
 module Egg.Runtime.Token where
 
-import Data.List (List)
 import Data.BigInt (BigInt)
+import Data.Foldable (foldMap)
+import Data.List (List)
+import Data.Show (show)
 
 data Token
     = Op String
@@ -10,3 +12,11 @@ data Token
     | Ch String
     | BInt BigInt
     | Num Number
+
+showToken :: Token -> String
+showToken (Op name)    = name
+showToken (Arr tokens) = foldMap showToken tokens
+showToken (Str str)    = str
+showToken (Ch ch)      = ch
+showToken (BInt int)   = show int
+showToken (Num num)    = show num
