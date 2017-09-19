0e296581c4227da8b632014d828deca7a9987391
Forest Belton <forest@homolo.gy>
2017-09-18 23:38:58 -0700

    Add ternary value operator

--- lib/runtime/operator/Caret.purs
+++ lib/runtime/operator/Caret.purs
@@ -4,6 +4,7 @@ import Data.List (fromFoldable)
 import Data.Number.Approximate (eqApproximate)
 import Prelude (($), (<>))
 
+import Egg.Runtime.Context (push)
 import Egg.Runtime.Operator.Embed
 import Egg.Runtime.Operator.Operator (Operator)
 import Egg.Runtime.Type (Ty(..))
@@ -16,6 +17,11 @@ caret =
           , description: "Evaluates the first block if the number is nonzero, otherwise the second."
           , body: ternaryOp $ \(ABlock t) (ABlock f) (n :: Number) ctx -> let block = fromFoldable $ if eqApproximate n 0.0 then f else t
                 in ctx { tokens = block <> ctx.tokens }
+          }
+        , { sig: [TAny, TAny, TNum]
+          , description: "Pushes the first value if the number is nonzero, otherwise the second."
+          , body: ternaryOp $ \(Any t) (Any f) (n :: Number) ctx -> let value = if eqApproximate n 0.0 then f else t
+                in push ctx value
           }
         ]
     }
