3c26862fbab5d8ffeb79f60dd5ebfa72dc2c4911
Forest Belton <forest@homolo.gy>
2017-09-19 12:03:23 -0700

    Finish reimplementing equals

--- lib/runtime/operator/Equals.purs
+++ lib/runtime/operator/Equals.purs
@@ -1,9 +1,7 @@
 module Egg.Runtime.Operator.Equals where
 
 import Egg.Runtime.Stmt
-import Data.BigInt (BigInt)
 import Data.Number.Approximate (eqApproximate)
-import Data.Unit (Unit)
 import Egg.Runtime.Operator.Operator (Operator)
 import Egg.Runtime.Type (Ty(..))
 import Prelude (($), (==), bind)
@@ -18,19 +16,20 @@ equals =
               x <- popBInt
               y <- popBInt
               push $ x == y
-        }
-
-          {- { sig: [TBInt, TBInt]
-          , description: "Integer equality."
-          , body: binaryOp $ \(x :: Int) (y :: Int) ctx -> push ctx $ lift (x == y)
           }
         , { sig: [TStr, TStr]
           , description: "String equality."
-          , body: binaryOp $ \(x :: String) (y :: String) ctx -> push ctx $ lift (x == y)
+          , body: do
+              x <- popStr
+              y <- popStr
+              push $ x == y
           }
         , { sig: [TNum, TNum]
           , description: "Decimal approximate equality."
-          , body: binaryOp $ \(x :: Number) (y :: Number) ctx -> push ctx $ lift $ eqApproximate x y
+          , body: do
+              x <- popNum
+              y <- popNum
+              push $ eqApproximate x y
           }
-        -}]
+        ]
     }
