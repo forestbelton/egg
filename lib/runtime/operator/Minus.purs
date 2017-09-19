de86777593a4d6d8e4bee50e57cf0c1e65773cb0
Forest Belton <forest@homolo.gy>
2017-09-19 01:09:16 -0700

    Add substring remove operator

--- lib/runtime/operator/Minus.purs
+++ lib/runtime/operator/Minus.purs
@@ -1,10 +1,13 @@
 module Egg.Runtime.Operator.Minus where
 
+import Data.String (replaceAll, Pattern(..), Replacement(..))
+import Prelude (($), (-))
+
 import Egg.Runtime.Operator.Embed
 import Egg.Runtime.Context (push)
 import Egg.Runtime.Operator.Operator (Operator)
 import Egg.Runtime.Type (Ty(..))
-import Prelude (($), (-))
+
 
 minus :: Operator
 minus =
@@ -17,6 +20,10 @@ minus =
         , { sig: [TNum, TNum]
           , description: "Decimal subtraction."
           , body: binaryOp $ \(x :: Number) (y :: Number) ctx -> push ctx $ lift (x - y)
+          }
+        , { sig: [TStr, TStr]
+          , description: "Remove all occurrences of the second string from the first."
+          , body: binaryOp $ \(haystack :: String) (needle :: String) ctx -> push ctx $ lift (replaceAll (Pattern needle) (Replacement "") haystack)
           }
         ]
     }
