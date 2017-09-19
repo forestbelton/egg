d3577d27a93901389f33711eabbe8b3ab7f4916e
Forest Belton <forest@homolo.gy>
2017-09-19 11:03:42 -0700

    Add remaining statement constructors

--- lib/runtime/Stmt.purs
+++ lib/runtime/Stmt.purs
@@ -1,15 +1,31 @@
 module Egg.Runtime.Stmt where
 
-import Control.Monad.Free
-
+import Control.Monad.Free (Free, liftF)
 import Data.BigInt (BigInt)
+import Prelude (($), Unit, unit, id)
+
 import Egg.Runtime.Token (Token)
 
 data StmtF a
-    = PopBigInt (BigInt -> a)
+    = PopBInt (BigInt -> a)
     | Execute (Array Token) a
     | Push Token a
     | Display String a
     | Read (String -> a)
 
 type Stmt a = Free StmtF a
+
+popBInt :: Stmt BigInt
+popBInt = liftF $ PopBInt id
+
+execute :: Array Token -> Stmt Unit
+execute block = liftF $ Execute block unit
+
+push :: Token -> Stmt Unit
+push tok = liftF $ Push tok unit
+
+display :: String -> Stmt Unit
+display str = liftF $ Display str unit
+
+read :: Stmt String
+read = liftF $ Read id
