21599f022fba832f4c4eddfc64a23dd8042d1ac1
Forest Belton <forest@homolo.gy>
2017-09-19 13:48:43 -0700

    Reimplement if-then-else operators

--- lib/runtime/operator/Caret.purs
+++ lib/runtime/operator/Caret.purs
@@ -2,25 +2,34 @@ module Egg.Runtime.Operator.Caret where
 
 import Data.List (fromFoldable)
 import Data.Number.Approximate (eqApproximate)
-import Prelude (($), (<>))
-
-import Egg.Runtime.Context (push)
+import Egg.Runtime.Embed (ABlock(..))
 import Egg.Runtime.Operator.Operator (Operator)
+import Egg.Runtime.Stmt
+import Egg.Runtime.Token (Token)
 import Egg.Runtime.Type (Ty(..))
+import Prelude (($), (<>), bind)
 
 caret :: Operator
 caret =
     { name: "^"
     , clauses:
-        [{- { sig: [TBlock, TBlock, TNum]
+        [ { sig: [TBlock, TBlock, TNum]
           , description: "Evaluate the first block if the number is nonzero, otherwise the second."
-          , body: ternaryOp $ \(ABlock t) (ABlock f) (n :: Number) ctx -> let block = fromFoldable $ if eqApproximate n 0.0 then f else t
-                in ctx { tokens = block <> ctx.tokens }
+          , body: do
+              n :: Number <- pop
+              ABlock f <- pop
+              ABlock t <- pop
+
+              execute $ if eqApproximate n 0.0 then t else f
           }
         , { sig: [TAny, TAny, TNum]
           , description: "Push the first value if the number is nonzero, otherwise the second."
-          , body: ternaryOp $ \(Any t) (Any f) (n :: Number) ctx -> let value = if eqApproximate n 0.0 then f else t
-                in push ctx value
+          , body: do
+              n :: Number <- pop
+              f :: Token <- pop
+              t :: Token <- pop
+
+              push $ if eqApproximate n 0.0 then t else f
           }
-        -}]
+        ]
     }
