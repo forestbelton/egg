f9ceae1a76af0e2bf9aca1b6aea0c31122faff23
Forest Belton <forest@homolo.gy>
2017-09-19 20:13:30 -0700

    Add if-else integer variants

--- lib/runtime/operator/Caret.purs
+++ lib/runtime/operator/Caret.purs
@@ -1,18 +1,30 @@
 module Egg.Runtime.Operator.Caret where
 
+import Data.BigInt (BigInt)
 import Data.Number.Approximate (eqApproximate)
+import Prelude (($), (==), bind, zero)
+
 import Egg.Runtime.Embed (ABlock(..))
 import Egg.Runtime.Operator.Operator (Operator)
 import Egg.Runtime.Stmt
 import Egg.Runtime.Token (Token)
 import Egg.Runtime.Type (Ty(..))
-import Prelude (($), bind)
+
 
 caret :: Operator
 caret =
     { name: "^"
     , clauses:
-        [ { sig: [TBlock, TBlock, TNum]
+        [ { sig: [TBlock, TBlock, TBInt]
+          , description: "Evaluate the first block if the integer is nonzero, otherwise the second."
+          , body: do
+              n :: BigInt <- pop
+              ABlock f <- pop
+              ABlock t <- pop
+
+              execute $ if n == zero then t else f
+          }
+        , { sig: [TBlock, TBlock, TNum]
           , description: "Evaluate the first block if the number is nonzero, otherwise the second."
           , body: do
               n :: Number <- pop
@@ -20,6 +32,15 @@ caret =
               ABlock t <- pop
 
               execute $ if eqApproximate n 0.0 then t else f
+          }
+        , { sig: [TAny, TAny, TBInt]
+          , description: "Push the first value if the integer is nonzero, otherwise the second."
+          , body: do
+              n :: BigInt <- pop
+              f :: Token <- pop
+              t :: Token <- pop
+
+              push $ if n == zero then t else f
           }
         , { sig: [TAny, TAny, TNum]
           , description: "Push the first value if the number is nonzero, otherwise the second."
